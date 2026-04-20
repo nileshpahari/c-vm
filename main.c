e85de65b78828d3de9ee9d77eb274931aabc6997
Nilesh Kumar Pahari <nileshpahari@protonmail.com>
2026-04-21 05:29:06 +0530

    add code

--- main.c
+++ main.c
@@ -1,24 +1,26 @@
+#include <inttypes.h>
 #include <stdint.h>
-#include <unitypes.h>
+
+typedef uint16_t u16;
 
 // memory structure define
-uint16_t PC_START = 0x3000;
-uint16_t mem[UINT16_MAX + 1] = {0};
+const u16 PC_START = 0x3000;
+u16 mem[UINT16_MAX + 1] = {0};
 
 // mem helper fn
-static inline uint16_t mr(uint16_t address) { return mem[address]; }
+static inline u16 mr(u16 address) { return mem[address]; }
 
-static inline void mw(uint16_t address, uint16_t val) { mem[address] = val; }
+static inline void mw(u16 address, u16 val) { mem[address] = val; }
 
 // registers define
 enum regist { R0 = 0, R1, R2, R3, R4, R5, R6, R7, RPC, RCND, RCNT };
-uint16_t reg[RCNT] = {0};
+u16 reg[RCNT] = {0};
 
 // IX system
 #define OPC(i) ((i) >> 12)
 
 #define NOPS (16) // Total possible instructions
-typedef void (*op_ex_f)(uint16_t instruction);
+typedef void (*op_ex_f)(u16 instruction);
 
 enum flags { FP = 1 << 0, FZ = 1 << 1, FN = 1 << 2 };
 
@@ -33,35 +35,92 @@ static inline void uf(enum regist r) {
 
 #define DR1(i) ((i >> 9) & 0x7) // destination reg
 #define SR1(i) ((i >> 6) & 0x7) // src reg 1
-#define SR2(i) ((i) & 0x7) // src reg 2
-#define FIMM(i) ((i >> 5) & 1) // immediate mode flag
-#define IMM(i) ((i) & 0x1F)    // extract 5-bit immediate
+#define SR2(i) ((i) & 0x7)      // src reg 2
+#define FIMM(i) ((i >> 5) & 1)  // immediate mode flag
+#define IMM(i) ((i) & 0x1F)     // extract 5-bit immediate
 
-static inline uint16_t sext(uint16_t n, int b) {
+static inline u16 sext(u16 n, int b) {
   return ((n >> (b - 1)) & 1) ? (n | (0xFFFF << b)) : n;
 }
 
 #define SEXTMM(i) sext(IMM(i), 5) // sign-extend IMM5
 
 // IXs
-static inline void add(uint16_t i) {
-	reg[DR1(i)] = reg[SR1(i)] + (FIMM(i) ? SEXTMM(i) : reg[SR2(i)]);
-	uf(DR1(i));
-}
-
-static inline void and(uint16_t i) {
-	reg[DR1(i)] = reg[SR1(i)] + (FIMM(i) ? SEXTMM(i) : reg[SR2(i)]);
-	uf(DR1(i));
-}
-
-
-
-// Function pointer array for instruction execution
-// op_ex_f op_ex[NOPS] = {br,  add, ld,  st,  jsr, and, ldr, str,
-//                        rti, not, ldi, sti, jmp, res, lea, trap};
 //
 
-int main() {
+// Mathematical Operations
+static inline void add(u16 i) {
+  reg[DR1(i)] = reg[SR1(i)] + (FIMM(i) ? SEXTMM(i) : reg[SR2(i)]);
+  uf(DR1(i));
+}
+
+static inline void and(u16 i) {
+  reg[DR1(i)] = reg[SR1(i)] + (FIMM(i) ? SEXTMM(i) : reg[SR2(i)]);
+  uf(DR1(i));
+}
+
+static inline void not(u16 i) {
+	reg[DR1(i)] = ~reg[SR1(i)];
+	uf(DR1(i));
+}
+
+#define POFF11(i) sext((i) & 0x7FF, 11)
+#define POFF9(i) sext((i) & 0x1FF, 9)
+#define POFF6(i) sext((i) & 0x3F, 6)
+
+#define FL(i) ((i >> 10) & 1)
+
+// Memory Loading
+static inline void ld(u16 i) {
+  reg[DR1(i)] = mr(reg[RPC] + POFF9(i));
+  uf(reg[DR1(i)]);
+}
+
+static inline void ldi(u16 i) {
+  reg[DR1(i)] = mr(mr(reg[RPC] + POFF9(i)));
+  uf(reg[DR1(i)]);
+}
+
+static inline void ldr(u16 i) {
+	reg[DR1(i)] = mr(reg[SR1(i)] + POFF6(i));
+	uf(reg[DR1(i)]);
+}
+
+static inline void lea(u16 i) {
+	reg[DR1(i)] = reg[RPC] + POFF9(i);
+	uf(DR1(i));
+}
+
+// Memory Storage
+static inline void st(u16 i) {
+	mw(reg[RPC] + POFF9(i), reg[DR1(i)]);
+}
+
+static inline void sti(u16 i) {
+	mw(mr(reg[RPC] + POFF9(i)), reg[DR1(i)]);
+}
+
+static inline void str(u16 i) {
+	mw(reg[SR1(i)] + POFF6(i), reg[DR1(i)]);
+}
+
+// Control Flow
+static inline void jmp(u16 i) {
+	reg[RPC] = reg[SR1(i)];
+}
+
+static inline void jsr(u16 i) {
+	reg[R7] = reg[RPC];
+	reg[RPC] = (FL(i)) ? (reg[RPC] + POFF11(i)) : reg[SR1(i)];
+}
+
+static inline void br(u16 i) {
 
 }
 
+// Function pointer array for instruction execution
+op_ex_f op_ex[NOPS] = {br,  add, ld,  st,  jsr, and, ldr, str,
+                       rti, not, ldi, sti, jmp, res, lea, trap};
+
+
+int main() {}
