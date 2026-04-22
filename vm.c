3643f0093ad9108b915d38e535eeeab5e5681a85
Nilesh Kumar Pahari <nileshpahari@protonmail.com>
2026-04-22 05:32:52 +0530

    update code

--- vm.c
+++ vm.c
@@ -1,11 +1,18 @@
 #include <inttypes.h>
+#include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef uint16_t u16;
 
 // memory structure define
 const u16 PC_START = 0x3000;
 u16 mem[UINT16_MAX + 1] = {0};
+
+// VM execution
+bool running = true;
 
 // mem helper fn
 static inline u16 mr(u16 address) { return mem[address]; }
@@ -60,8 +67,8 @@ static inline void and(u16 i) {
 }
 
 static inline void not(u16 i) {
-	reg[DR1(i)] = ~reg[SR1(i)];
-	uf(DR1(i));
+  reg[DR1(i)] = ~reg[SR1(i)];
+  uf(DR1(i));
 }
 
 #define POFF11(i) sext((i) & 0x7FF, 11)
@@ -84,47 +91,130 @@ static inline void ldi(u16 i) {
 }
 
 static inline void ldr(u16 i) {
-	reg[DR1(i)] = mr(reg[SR1(i)] + POFF6(i));
-	uf(reg[DR1(i)]);
+  reg[DR1(i)] = mr(reg[SR1(i)] + POFF6(i));
+  uf(reg[DR1(i)]);
 }
 
 static inline void lea(u16 i) {
-	reg[DR1(i)] = reg[RPC] + POFF9(i);
-	uf(DR1(i));
+  reg[DR1(i)] = reg[RPC] + POFF9(i);
+  uf(DR1(i));
 }
 
 // Memory Storage
-static inline void st(u16 i) {
-	mw(reg[RPC] + POFF9(i), reg[DR1(i)]);
-}
+static inline void st(u16 i) { mw(reg[RPC] + POFF9(i), reg[DR1(i)]); }
 
-static inline void sti(u16 i) {
-	mw(mr(reg[RPC] + POFF9(i)), reg[DR1(i)]);
-}
+static inline void sti(u16 i) { mw(mr(reg[RPC] + POFF9(i)), reg[DR1(i)]); }
 
-static inline void str(u16 i) {
-	mw(reg[SR1(i)] + POFF6(i), reg[DR1(i)]);
-}
+static inline void str(u16 i) { mw(reg[SR1(i)] + POFF6(i), reg[DR1(i)]); }
 
 // Control Flow
-static inline void jmp(u16 i) {
-	reg[RPC] = reg[SR1(i)];
-}
+static inline void jmp(u16 i) { reg[RPC] = reg[SR1(i)]; }
 
 static inline void jsr(u16 i) {
-	reg[R7] = reg[RPC];
-	reg[RPC] = (FL(i)) ? (reg[RPC] + POFF11(i)) : reg[SR1(i)];
+  reg[R7] = reg[RPC];
+  reg[RPC] = (FL(i)) ? (reg[RPC] + POFF11(i)) : reg[SR1(i)];
 }
 
 static inline void br(u16 i) { // OPCODE [n z p] [offset9]
-	if (reg[RCND] & FCND(i)) {
-		reg[RPC] = reg[RPC] + POFF11(i);
-	}
+  if (reg[RCND] & FCND(i)) {
+    reg[RPC] = reg[RPC] + POFF11(i);
+  }
 }
 
-// Function pointer array for instruction execution
-op_ex_f op_ex[NOPS] = {br,  add, ld,  st,  jsr, and, ldr, str,
-                       rti, not, ldi, sti, jmp, res, lea, trap};
+// Trap
+//
+#define TRP(i) ((i) & 0xFF)
 
+enum { trp_offset = 0x20 };
+typedef void (*trp_ex_f)();
 
-int main() {}
+// | Function  | Vector | Description                              |
+// | `tgetc`   | 0x20   | Read a character into R0                 |
+// | `tout`    | 0x21   | Print character in R0                    |
+// | `tputs`   | 0x22   | Print string from memory (address in R0) |
+// | `tin`     | 0x23   | Read + echo character                    |
+// | `tputsp`  | 0x24   | (Not implemented)                        |
+// | `thalt`   | 0x25   | Stop execution                           |
+// | `tinu16`  | 0x26   | Read 16-bit integer                      |
+// | `toutu16` | 0x27   | Print 16-bit integer                     |
+//
+
+static inline void tgetc() { reg[R0] = getchar(); }
+
+static inline void tout() { fprintf(stdout, "%c", (char)reg[R0]); }
+
+static inline void tputs() {
+  uint16_t *p = mem + reg[R0]; //  //
+  while (*p) {
+    fprintf(stdout, "%c", (char)*p);
+    p++;
+  }
+}
+
+static inline void tin() {
+  reg[R0] = getchar();
+  fprintf(stdout, "%c", (char)reg[R0]);
+}
+
+static inline void thalt() { running = false; }
+
+static inline void tinu16() { fscanf(stdin, "%hu", &reg[R0]); }
+
+static inline void tputsp() {}
+
+static inline void toutu16() { fprintf(stdout, "%hu\n", reg[R0]); }
+
+trp_ex_f trp_ex[8] = {tgetc, tout, tputs, tin, tputsp, thalt, tinu16, toutu16};
+
+static inline void trap(uint16_t i) { trp_ex[TRP(i) - trp_offset](); }
+
+// Function pointer array for instruction execution // TODO: impl `res` and
+// `rti`
+op_ex_f op_ex[NOPS] = {br,  add, ld,  st,  jsr, and, ldr,
+                       str, not, ldi, sti, jmp, lea, trap};
+
+void start(uint16_t offset) {
+  // Initialize RPC to program start
+  reg[RPC] = PC_START + offset;
+
+  while (running) {
+    // FETCH
+    uint16_t i = mr(reg[RPC]++);
+
+    // DECODE + EXECUTE
+    op_ex[OPC(i)](i);
+  }
+}
+
+void id_img(char *fname, uint16_t offset) {
+  char *check = strstr(fname, ".obj");
+  if (check == NULL) {
+    printf("\n The provided file is not .obj, Pls pass a .obj file\n");
+    exit(1);
+  }
+
+  FILE *in = fopen(fname, "rb");
+  if (in == NULL) {
+    fprintf(stderr, "cannot open file %s.\n", fname);
+    exit(1);
+  }
+
+  uint16_t *p = mem + PC_START + offset;
+  fread(p, sizeof(uint16_t), (UINT16_MAX - PC_START), in);
+  fclose(in);
+}
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s <program.obj>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  // Load the program into memory
+  id_img(argv[1], 0x0);
+
+  // Start execution
+  start(0x0);
+
+  return 0;
+}
